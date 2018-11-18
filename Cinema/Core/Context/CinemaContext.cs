7bbd897197258e3fb1fd01311d9fa63eef456313
Alex Ciurariu <alexandru.ciurariu@assist.ro>
2018-11-18 16:44:59 +0200

    Added Context and DesignTimeContext

---
+++ Cinema/Core/Context/CinemaContext.cs
@@ -0,0 +1,32 @@
+﻿using Core.Configurations;
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Context
+{
+    public sealed class CinemaContext : DbContext
+    {
+        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Auditorium> Auditoriums { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<CinemaMovie> CinemaMovies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new AuditoriumConfiguration());
+            builder.ApplyConfiguration(new BookingConfiguration());
+            builder.ApplyConfiguration(new CinemaConfiguration());
+            builder.ApplyConfiguration(new EventConfiguration());
+            builder.ApplyConfiguration(new MovieConfiguration());
+            builder.ApplyConfiguration(new CinemaMovieConfiguration());
+        }
+    }
+}
