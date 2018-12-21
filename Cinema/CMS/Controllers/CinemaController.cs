7e70b62990a669f5425afaa2c97c1620edb4b226
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2018-12-21 20:25:34 +0200

    Implemented Cinema Controller

---
+++ Cinema/CMS/Controllers/CinemaController.cs
@@ -0,0 +1,118 @@
+﻿using AutoMapper;
+using CMS.Models.Cinema;
+using Core.Interfaces;
+using Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CMS.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly ICinemaService cinemaService;
+        private readonly IMapper mapper;
+
+        public CinemaController(
+            ICinemaService cinemaService,
+            IMapper mapper)
+        {
+            this.cinemaService = cinemaService;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cinemas = await cinemaService.GetAllAsync();
+            var result = mapper.Map<List<CinemaIndexViewModel>>(cinemas);
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var cinema = await cinemaService.GetByIdAsync(id);
+            var result = mapper.Map<CinemaDetailsViewModel>(cinema);
+
+            return View(result);
+        }
+
+        #pragma warning disable CS1998
+        public async Task<IActionResult> Create()
+        #pragma warning restore CS1998
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CinemaCreateViewModel dto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(dto);
+                }
+
+                var cinema = mapper.Map<Cinema>(dto);
+                await cinemaService.CreateAsync(cinema);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(dto);
+            }
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var cinema = await cinemaService.GetByIdAsync(id);
+            var result = mapper.Map<CinemaEditViewModel>(cinema);
+
+            return View(result);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CinemaEditViewModel dto)
+        {
+            try
+            {
+                var cinema = mapper.Map<Cinema>(dto);
+                await cinemaService.UpdateAsync(cinema);
+
+                return RedirectToAction(nameof(Details), new { id = cinema.Id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var cinema = await cinemaService.GetByIdAsync(id);
+            var result = mapper.Map<CinemaDeleteViewModel>(cinema);
+
+            return View(result);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(CinemaDeleteViewModel dto)
+        {
+            try
+            {
+                var cinema = await cinemaService.GetByIdAsync(dto.Id);
+                await cinemaService.DeleteAsync(cinema);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
