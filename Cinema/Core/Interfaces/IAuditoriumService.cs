b6a1e3227feab5328cbc06b60eb20341fd4afb1a
sebastian_mihai <mihai.sumanariu@assist.ro>
2019-02-05 10:40:53 +0200

    Auditorium Context + Service

--- Cinema/Core/Interfaces/IAuditoriumService.cs
+++ Cinema/Core/Interfaces/IAuditoriumService.cs
@@ -1,9 +1,19 @@
 ﻿using Core.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Core.Interfaces
 {
     public interface IAuditoriumService : IBaseService<Auditorium, Guid>
     {
-    }
+		Task<List<Auditorium>> GetEagerAllAsync();
+		Task<Auditorium> GetEagerByIdAsync(Guid id);
+		Task<List<Auditorium>> GetEagerByIdsAsync(List<Guid> ids);
+
+		IQueryable<Auditorium> GetPagedQuery(string orderBy, bool order);
+		Task<List<Auditorium>> GetPagedAsync(IQueryable<Auditorium> query, int page, int size);
+		Task<int> GetPagedCountAsync(IQueryable<Auditorium> pagedQuery);
+	}
 }
--- Cinema/Core/Services/AuditoriumService.cs
+++ Cinema/Core/Services/AuditoriumService.cs
@@ -1,14 +1,93 @@
 ﻿using Core.Context;
 using Core.Interfaces;
 using Core.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Core.Services
 {
-    public class AuditoriumService : BaseService<Auditorium, Guid>, IAuditoriumService
-    {
-        public AuditoriumService(CinemaContext context) : base(context)
-        {
-        }
-    }
+	public class AuditoriumService : BaseService<Auditorium, Guid>, IAuditoriumService
+	{
+		public AuditoriumService(CinemaContext context) : base(context)
+		{
+		}
+
+		public async Task<List<Auditorium>> GetEagerAllAsync()
+		{
+			return await context
+			   .Auditoriums
+			   .AsNoTracking()
+			   .Include(auditorium => auditorium.Cinema)
+			   .Include(auditorium => auditorium.Events)
+			   .ToListAsync();
+		}
+
+		public async Task<Auditorium> GetEagerByIdAsync(Guid id)
+		{
+			return await context
+			   .Auditoriums
+			   .AsNoTracking()
+			   .Include(auditorium => auditorium.Cinema)
+			   .Include(auditorium => auditorium.Events)
+			   .FirstOrDefaultAsync(auditorium => auditorium.Id.Equals(id));
+		}
+
+		public async Task<List<Auditorium>> GetEagerByIdsAsync(List<Guid> ids)
+		{
+			return await context
+				.Auditoriums
+				.AsNoTracking()
+				.Include(auditorium => auditorium.Cinema)
+				.Include(auditorium => auditorium.Events)
+				.Join(
+					ids,
+					auditorium => auditorium.Id,
+					id => id,
+					(auditorium, id) => auditorium)
+				.ToListAsync();
+		}
+
+		public IQueryable<Auditorium> GetPagedQuery(string orderBy, bool order)
+		{
+			var query = GetBaseQuery();
+
+			////string filter = "";
+			////query = query.Where(cinema =>
+			////    cinema.Name.Contains(filter)
+			////    || cinema.Address.Contains(filter)
+			////    || cinema.Contact.Contains(filter)
+			////    || cinema.Location.Contains(filter));
+
+			switch (orderBy)
+			{
+				case "Name" when order is true:
+					query = query.OrderBy(auditorium => auditorium.Name);
+					break;
+				case "Name" when order is false:
+					query = query.OrderByDescending(auditorium => auditorium.Name);
+					break;
+				default:
+					break;
+			}
+
+			return query;
+		}
+
+		public async Task<List<Auditorium>> GetPagedAsync(IQueryable<Auditorium> query, int page, int size)
+		{
+			return await query
+				.Skip(page * size)
+				.Take(size)
+				.ToListAsync();
+		}
+
+		public async Task<int> GetPagedCountAsync(IQueryable<Auditorium> pagedQuery)
+		{
+			return await pagedQuery.CountAsync();
+		}
+	}
 }
+
